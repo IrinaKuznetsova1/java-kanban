30bfac3157f065da6a9be4ac9cf2caee096753dc
Irina Kuznetsova <dyachenkoia@yandex.ru>
2025-01-15 20:07:16 +0300

    в тесте getHistory() get() заменен на getFirst(),  в тесте maxSizeofHistoryShouldBeTen() assertTrue() заменен на assertEquals()

--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -7,7 +7,7 @@ import model.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,7 +24,7 @@ class InMemoryHistoryManagerTest {
     public void addTask() {
         Task task1 = new Task(1, "Test Title 1", "Test Description 1", Status.NEW);
         historyManager.addTask(task1);
-        final ArrayList<Task> testHistory = historyManager.getHistory();
+        final List<Task> testHistory = historyManager.getHistory();
         assertNotNull(testHistory, "Task1 не сохранен.");
         assertEquals(1, testHistory.size(), "Task1 не сохранен.");
     }
@@ -33,8 +33,8 @@ class InMemoryHistoryManagerTest {
     public void getHistory() {
         Task task1 = new Task(1, "Test Title 1", "Test Description 1", Status.NEW);
         historyManager.addTask(task1);
-        final ArrayList<Task> testHistory = historyManager.getHistory();
-        final Task savedTask = testHistory.get(0);
+        final List<Task> testHistory = historyManager.getHistory();
+        final Task savedTask = testHistory.getFirst();
         assertEquals(savedTask.getTitle(), task1.getTitle(), "Названия объектов не совпадают.");
         assertEquals(savedTask.getDescription(), task1.getDescription(), "Описания объектов не совпадают.");
         assertEquals(savedTask.getStatus(), task1.getStatus(), "Статусы объектов не совпадают.");
@@ -49,7 +49,7 @@ class InMemoryHistoryManagerTest {
         historyManager.addTask(task1);
         historyManager.addTask(epic1);
         historyManager.addTask(subtask1);
-        final ArrayList<Task> testHistory = historyManager.getHistory();
+        final List<Task> testHistory = historyManager.getHistory();
         final Task savedTask = testHistory.get(0);
         assertEquals(savedTask.getClass(), Task.class, "Класс объекта изменился после сохранения.");
         final Task savedEpic = testHistory.get(1);
@@ -64,7 +64,7 @@ class InMemoryHistoryManagerTest {
         for (int i = 1; i <= 11; i++) {
             historyManager.addTask(task1);
         }
-        assertTrue(historyManager.getHistory().size() == 10);
+        assertEquals(10, historyManager.getHistory().size());
 
         Epic epic1 = new Epic(2, "Test Title 2", "Test Description 2");
         historyManager.addTask(epic1);
